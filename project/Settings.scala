6cc5f93dbafd86515aed3acfcf4ffbc59944b738
Steffen Klug <45033201+stklug84@users.noreply.github.com>
2021-01-12 23:48:13 +0100

    Remove obsolete sbt settings

--- project/Settings.scala
+++ project/Settings.scala
@@ -1,10 +1,6 @@
 import Dependencies._
 import sbt.Keys._
 import sbt._
-import sbtassembly.AssemblyPlugin.autoImport._
-import com.typesafe.sbt.SbtGit._
-import com.scalapenos.sbt.prompt._
-import SbtPrompt.autoImport._
 
 /** Definition of settings for build instances
   *
@@ -12,21 +8,6 @@ import SbtPrompt.autoImport._
   *
   */
 object Settings {
-  /** General project related startup configuration
-    *
-    * Operations are only executed during startup process
-    *
-    */
-  lazy val startupTransition: State => State = {
-    s: State => "writeHooks" :: s
-  }
-
-  lazy val startupSettings = Seq(
-    Global / onLoad := {
-      val old = (Global / onLoad).value
-      startupTransition compose old
-    }
-  )
 
   /** General project related configuration
     *
@@ -108,72 +89,5 @@ object Settings {
     // Publishing path
     publishTo :=
       Some("Artifactory Realm" at "<>"),
-
-    /** sbt-assembly plugin configuration
-      *
-      * Configuration for creating assemblies/fat/uber jars
-      *
-      * @param assembly / assemblyOption : Define rules of how to generate assemblies
-      * @param assembly / assemblyMergeStrategy: Define rules which library to pick in case of duplicates
-      *r
-      */
-    // Avoid adding scala libraries to assemblies
-    assembly / assemblyOption := (assembly / assemblyOption).value.copy(includeScala = false),
-    // Pick the first encountered by the assembler (this is the default strategy)
-    assembly / assemblyMergeStrategy := {
-      // Needed for 3rd party data sources such as Delta or Kafka to work properly
-      case "META-INF/services/org.apache.spark.sql.sources.DataSourceRegister" =>
-        MergeStrategy.concat
-      // Ignore duplicates not used by Java 8
-      case "module-info.class" => MergeStrategy.discard
-      // Conflicts can be solved by overriding dependencies
-      //case PathList("com","sun", xs @ _*) => MergeStrategy.first
-      //case PathList("com","fasterxml", xs @ _*) => MergeStrategy.first
-      //case PathList("com","amazonaws", xs @ _*) => MergeStrategy.first
-      // Conflicting meta-information except for Spark data sources should be discarded
-      case PathList("META-INF", xs @ _*) => MergeStrategy.discard
-      case x =>
-        val oldStrategy = (assembly / assemblyMergeStrategy).value
-        oldStrategy(x)
-    },
-    // Exclude full jars when building an assembly
-    assemblyExcludedJars in assembly := {
-      val cp = (fullClasspath in assembly).value
-      cp filter {_.data.getName == "aws-java-sdk-bundle-1.11.271.jar"}
-      // cp filter {_.data.getName == "httpclient-4.5.3.jar"}
-    },
-
-    /** sbt-git plugin configuration
-      *
-      * Prerequisite: {{{ import com.typesafe.sbt.SbtGit._ }}}
-      *
-      * @param git.gitTagToVersionNumber: Redefines default version tag detection for tags without
-      *
-      * Enable plugin in build.sbt per project via {{{enablePlugins(GitVersioning, GitBranchPrompt)}}}
-      *
-      */
-    git.gitTagToVersionNumber := {
-      tag: String =>
-      if(tag matches "[0-9]+\\..*") Some(tag)
-      else None
-    },
-
-    /** sbt-prompt configuration
-      *
-      * Prerequisite: {{{import com.scalapenos.sbt.prompt._
-      import SbtPrompt.autoImport._}}}
-      *
-      * @param promptTheme: Pick predefined theme
-      *
-      */
-    promptTheme := com.scalapenos.sbt.prompt.PromptThemes.ScalapenosTheme,
-
-    /** sbt-scapegoat plugin configuration
-      *
-      * Set scapegoat version, required for scapegoat to find correct sbt and scala version
-      *
-      * @param scapegoatVersion: Define scapegoat version to be used by project
-      */
-    //ThisBuild / scapegoatVersion := "1.3.8"
   )
 }
